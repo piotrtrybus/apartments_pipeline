5c94eed4a8b8a41ed36855953175c2fd93c59242
Piotr Trybus <p_trybus@wargaming.net>
2025-05-29 10:42:31 +0200

    Logger updates + unlock scraper

--- orchestration.py
+++ orchestration.py
@@ -3,30 +3,34 @@ from prague_apartments_ingestion.loader import load_data
 from prefect import flow, task, get_run_logger
 from dotenv import load_dotenv
 import subprocess
-import logging
 import os
-import sys
-
-#Logger setup
-logger = get_run_logger()
-logger.setLevel(logging.INFO)
-
 
 @task
 def extract():
-    logger.info("Initiating the web scraper")
-    scrape_apartments()
-    logger.info("Web scraping completed")
+    logger = get_run_logger()
+    try:
+        logger.info("Initiating the web scraper")
+        scrape_apartments()
+        logger.info("Web scraping completed")
+    except Exception as e:
+        logger.error(f"Scraping failed: {e}")
+        raise
 
 @task
 def load():
-    logger.info("Initiating the Postgres load")
-    load_data()
-    logger.info("Postgres load completed")
+    logger = get_run_logger()
+    try:
+        logger.info("Initiating the Postgres load")
+        load_data()
+        logger.info("Postgres load completed")
+    except Exception as e:
+        logger.error(f"Unable to load data: {e}")
+        raise
 
 
 @task
 def transform():
+    logger = get_run_logger()
     load_dotenv(dotenv_path="../prague_apartments_dbt/.env", override=True)
     logger.info("Initiating the DBT execution")
     try:
@@ -40,16 +44,26 @@ def transform():
             errors="replace"  
         )
         logger.info("DBT execution completed")
+        logger.debug(f"DBT stdout:\n{result.stdout}")
+        logger.debug(f"DBT stderr:\n{result.stderr}")
     except subprocess.CalledProcessError as e:
         logger.error(f"DBT failed with return code {e.returncode}")
+        logger.error(f"stderr: {e.stderr}")
+        raise
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
         raise
 
 
 @task
 def remove_csv():
-    logger.info("Removing file")
-    os.remove("data/prague_apartments.csv")
-
+    logger = get_run_logger()
+    try:
+        logger.info("Removing file")
+        os.remove("data/prague_apartments.csv")
+    except Exception as e:
+        logger.error("Unable to remove CSV file")
+        raise
 
 @flow
 def run_elt():
