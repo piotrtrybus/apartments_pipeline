21852d88b11dae9cbd7370d855cbdcdf6427157c
Piotr Trybus <p_trybus@wargaming.net>
2025-05-29 09:38:48 +0200

    Bug fix

--- orchestration.py
+++ orchestration.py
@@ -1,6 +1,7 @@
 from prague_apartments_scraper.scraper import scrape_apartments
 from prague_apartments_ingestion.loader import load_data
 from prefect import flow, task
+from dotenv import load_dotenv
 import subprocess
 import logging
 import os
@@ -46,6 +47,7 @@ def load_env_from_dotenv():
 
 @task
 def transform():
+    load_dotenv(dotenv_path="../prague_apartments_dbt/.env", override=True)
     logger.info("Initiating the DBT execution")
     try:
         result = subprocess.run(
